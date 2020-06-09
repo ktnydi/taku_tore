c5f1ac9af3fffe51cef67bb21049b80a01de4153
Yudai Kitano <kitano.prog@gmail.com>
2020-06-10 08:55:17 +0900

    Add remove user processing

--- lib/remove_user/remove_user_page.dart
+++ lib/remove_user/remove_user_page.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class RemoveUser extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _store = Firestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<bool> hasAvatar({FirebaseUser user}) async {
+    try {
+      await _storage.ref().child('/images/${user.uid}').getDownloadURL();
+      return true;
+    } catch (error) {
+      print(error);
+      return false;
+    }
+  }
+
+  void removeUser() async {
+    FirebaseUser currentUser = await _auth.currentUser();
+    if (currentUser != null) {
+      await _store.document('/users/${currentUser.uid}').delete();
+      if (await hasAvatar(user: currentUser)) {
+        _storage.ref().child('/images/${currentUser.uid}').delete();
+      }
+      await currentUser.delete();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,6 +71,43 @@ class RemoveUser extends StatelessWidget {
                 ),
                 onPressed: () {
                   // TODO: Implement processing for removing account.
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('本当に削除しますか？'),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('キャンセル'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          FlatButton(
+                            child: Text(
+                              '削除する',
+                              style: TextStyle(
+                                color: Colors.redAccent,
+                              ),
+                            ),
+                            onPressed: () async {
+                              try {
+                                removeUser();
+                              } catch (error) {
+                                print(error);
+                              } finally {
+                                // アラート非表示
+                                Navigator.pop(context);
+                              }
+                              // 設定画面に戻る
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
               ),
             ),
