5fcebf87e7985b010e7446585c78e9b21fa34d81
Yudai Kitano <kitano.prog@gmail.com>
2020-06-19 00:07:32 +0900

    Add user model

---
+++ lib/user.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class User {
+  User({
+    @required this.uid,
+    @required this.displayName,
+    @required this.photoURL,
+    @required this.isTeacher,
+    this.email,
+    this.about,
+    this.canDo,
+    this.recommend,
+    @required this.createdAt,
+  });
+
+  final String uid;
+  final String displayName;
+  final String photoURL;
+  final bool isTeacher;
+  final String email;
+  final String about;
+  final String canDo;
+  final String recommend;
+  final Timestamp createdAt;
+}
