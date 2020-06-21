5094de3babeb2a834cbd37a83551654d24839bda
Yudai Kitano <kitano.prog@gmail.com>
2020-06-21 09:37:30 +0900

    Add processing for adding consulting room

--- lib/teacher_detail/teacher_detail_model.dart
+++ lib/teacher_detail/teacher_detail_model.dart
@@ -6,6 +6,7 @@ import '../user.dart';
 class TeacherDetailModel extends ChangeNotifier {
   User teacher;
   bool isBookmarked = false;
+  bool isAlreadyExist = false;
 
   Future checkBookmark({User teacher}) async {
     final currentUser = await FirebaseAuth.instance.currentUser();
@@ -20,6 +21,19 @@ class TeacherDetailModel extends ChangeNotifier {
     final docs = await query.getDocuments();
     this.isBookmarked = docs.documents.isNotEmpty;
     notifyListeners();
+  }
+
+  Future checkRoom({User teacher}) async {
+    final currentUser = await FirebaseAuth.instance.currentUser();
+    final roomQuery = Firestore.instance
+        .collection('users')
+        .document(currentUser.uid)
+        .collection('rooms')
+        .where('member.teacherId', isEqualTo: teacher.uid);
+
+    final docs = await roomQuery.getDocuments();
+    final isExist = docs.documents.isNotEmpty;
+    this.isAlreadyExist = isExist;
   }
 
   Future addBookmark() async {
@@ -53,5 +67,33 @@ class TeacherDetailModel extends ChangeNotifier {
         .collection('bookmarks')
         .document(docId)
         .delete();
+  }
+
+  Future addRoom() async {
+    final currentUser = await FirebaseAuth.instance.currentUser();
+
+    if (teacher.uid == currentUser.uid) {
+      throw ('自分には相談できません。');
+    }
+
+    final roomRef = Firestore.instance
+        .collection('users')
+        .document(currentUser.uid)
+        .collection('rooms');
+
+    final roomQuery = roomRef.where('member.teacherId', isEqualTo: teacher.uid);
+    final docs = await roomQuery.getDocuments();
+    final isExist = docs.documents.isNotEmpty;
+
+    if (isExist) {
+      throw ('すでに相談済みです。');
+    }
+
+    await roomRef.add({
+      'member': {
+        'teacherId': teacher.uid,
+        'studentId': currentUser.uid,
+      },
+    });
   }
 }
--- lib/teacher_detail/teacher_detail_page.dart
+++ lib/teacher_detail/teacher_detail_page.dart
@@ -69,7 +69,9 @@ class TeacherDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TeacherDetailModel>(
-      create: (_) => TeacherDetailModel()..checkBookmark(teacher: teacher),
+      create: (_) => TeacherDetailModel()
+        ..checkBookmark(teacher: teacher)
+        ..checkRoom(teacher: teacher),
       child: Scaffold(
         appBar: AppBar(
           actions: <Widget>[
@@ -193,70 +195,138 @@ class Content extends StatelessWidget {
     );
   }
 
+  Future addRoom({TeacherDetailModel model, BuildContext context}) async {
+    try {
+      await model.addRoom();
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('部屋を追加しました。'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        },
+      );
+    } catch (error) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(error.toString()),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            teacher.displayName,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          SizedBox(height: 5),
-          Divider(),
-          SizedBox(height: 15),
-          Text(
-            '自己紹介',
-            style: _label(),
-          ),
-          SizedBox(height: 5),
-          Text(
-            teacher.about,
-            style: _description(),
-          ),
-          SizedBox(height: 15),
-          Text(
-            'できること',
-            style: _label(),
-          ),
-          SizedBox(height: 5),
-          Text(
-            teacher.canDo,
-            style: _description(),
-          ),
-          SizedBox(height: 15),
-          Text(
-            'こんな方におすすめ',
-            style: _label(),
-          ),
-          SizedBox(height: 5),
-          Text(
-            teacher.recommend,
-            style: _description(),
-          ),
-          SizedBox(height: 15),
-          ButtonTheme(
-            minWidth: double.infinity,
-            height: 50.0,
-            child: RaisedButton(
-              child: Text(
-                '相談する',
+    return Consumer<TeacherDetailModel>(
+      builder: (_, model, __) {
+        model.teacher = teacher;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                teacher.displayName,
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                  color: Colors.white,
+                  fontSize: 20,
                 ),
               ),
-              onPressed: () {},
-            ),
+              SizedBox(height: 5),
+              Divider(),
+              SizedBox(height: 15),
+              Text(
+                '自己紹介',
+                style: _label(),
+              ),
+              SizedBox(height: 5),
+              Text(
+                teacher.about,
+                style: _description(),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'できること',
+                style: _label(),
+              ),
+              SizedBox(height: 5),
+              Text(
+                teacher.canDo,
+                style: _description(),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'こんな方におすすめ',
+                style: _label(),
+              ),
+              SizedBox(height: 5),
+              Text(
+                teacher.recommend,
+                style: _description(),
+              ),
+              SizedBox(height: 15),
+              ButtonTheme(
+                minWidth: double.infinity,
+                height: 50.0,
+                child: RaisedButton(
+                  child: Text(
+                    !model.isAlreadyExist ? '相談する' : 'すでに相談済みです。',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: !model.isAlreadyExist
+                      ? () async {
+                          await showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('${teacher.displayName}さんと相談しますか？'),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text('キャンセル'),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                  FlatButton(
+                                    child: Text('OK'),
+                                    onPressed: () async {
+                                      Navigator.pop(context);
+                                      await addRoom(
+                                        model: model,
+                                        context: context,
+                                      );
+                                    },
+                                  )
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      : null,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
