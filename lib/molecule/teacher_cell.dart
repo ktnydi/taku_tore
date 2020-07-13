7deeca15643696a0814f914d42ed45fdb31d186e
Yudai Kitano <kitano.prog@gmail.com>
2020-07-13 11:35:21 +0900

    Modify teacher cell ui

--- lib/molecule/teacher_cell.dart
+++ lib/molecule/teacher_cell.dart
@@ -9,56 +9,87 @@ class TeacherCell extends StatelessWidget {
   final User teacher;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.transparent,
-        backgroundImage: NetworkImage(teacher.photoURL),
-        radius: 25,
-      ),
-      title: Text(
-        teacher.title,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(teacher.displayName),
-          Row(
+    return Material(
+      color: Colors.white,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => TeacherDetail(
+                teacher: teacher,
+              ),
+            ),
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              RatingBarIndicator(
-                rating: teacher.avgRating.toDouble(),
-                itemBuilder: (context, index) => Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-                itemCount: 5,
-                itemSize: 20,
+              CircleAvatar(
+                backgroundColor: Colors.transparent,
+                backgroundImage: NetworkImage(teacher.photoURL),
+                radius: 25,
               ),
-              SizedBox(width: 3),
-              Text(
-                '${teacher.avgRating}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+              SizedBox(width: 10),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      teacher.title,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      teacher.displayName,
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 13,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: <Widget>[
+                        RatingBarIndicator(
+                          rating: teacher.avgRating.toDouble(),
+                          itemBuilder: (context, index) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          itemCount: 5,
+                          itemSize: 20,
+                        ),
+                        SizedBox(width: 3),
+                        Text(
+                          '${teacher.avgRating}',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 3),
+                        Text(
+                          '(${teacher.numRatings})',
+                          style: TextStyle(
+                            color: Colors.black54,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(width: 3),
-              Text('(${teacher.numRatings})'),
             ],
           ),
-        ],
+        ),
       ),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (BuildContext context) => TeacherDetail(
-              teacher: teacher,
-            ),
-          ),
-        );
-      },
     );
   }
 }
