2cd7f4377a401a87775a6e487f3c7d19b07d0a67
Yudai Kitano <kitano.prog@gmail.com>
2020-07-29 22:48:21 +0900

    feature: add notice list ui

--- lib/presentation/notice_list/notice_list_model.dart
+++ lib/presentation/notice_list/notice_list_model.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:takutore/domain/notice.dart';
 
 class NoticeListModel extends ChangeNotifier {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  Firestore _store = Firestore.instance;
+  List<Notice> notices = [];
   bool isLoading = false;
 
   void beginLoading() {
@@ -16,11 +22,24 @@ class NoticeListModel extends ChangeNotifier {
   Future fetchNotices() async {
     this.beginLoading();
 
-    // Following code is dummy.
-    // Actually, You write code for fetching notices here.
-    await Future.delayed(
-      Duration(milliseconds: 1000),
+    final user = await _auth.currentUser();
+
+    final collection =
+        _store.collection('users').document(user.uid).collection('notices');
+
+    final docs = await collection.getDocuments();
+
+    final notices = await Future.wait(
+      docs.documents.map(
+        (doc) async {
+          final notice = Notice(doc);
+          await notice.fetchSender();
+          return notice;
+        },
+      ),
     );
+
+    this.notices = notices;
 
     this.endLoading();
   }
--- lib/presentation/notice_list/notice_list_page.dart
+++ lib/presentation/notice_list/notice_list_page.dart
@@ -1,8 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../domain/notice.dart';
 import 'notice_list_model.dart';
 
 class NoticeList extends StatelessWidget {
+  Widget _buildNoticeCell(BuildContext context, Notice notice) {
+    switch (notice.type) {
+      case 'add room':
+        return _buildAddRoomCell(context, notice);
+      default:
+        return SizedBox();
+    }
+  }
+
+  Widget _buildAddRoomCell(BuildContext context, Notice notice) {
+    return Material(
+      color: Colors.white,
+      child: InkWell(
+        onTap: () {
+          // TODO: Navigation to NoticeDetail.
+        },
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.transparent,
+                backgroundImage: NetworkImage(notice.sender.photoURL),
+              ),
+              SizedBox(width: 15),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                        ),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: notice.sender.displayName,
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: 'さんが${notice.message}',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      notice.createdAt,
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +83,25 @@ class NoticeList extends StatelessWidget {
                 child: CircularProgressIndicator(),
               );
             }
-            return Center(
-              child: Text(
-                '新しい通知はありません。',
-              ),
+
+            if (model.notices.isEmpty) {
+              return Center(
+                child: Text(
+                  '新しい通知はありません。',
+                ),
+              );
+            }
+
+            final notices = model.notices.map(
+              (notice) {
+                return _buildNoticeCell(context, notice);
+              },
+            ).toList();
+
+            return ListView.separated(
+              itemBuilder: (context, index) => notices[index],
+              separatorBuilder: (context, index) => Divider(height: 0.5),
+              itemCount: notices.length,
             );
           },
         ),
