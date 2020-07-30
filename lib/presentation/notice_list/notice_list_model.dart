3e9f8069ba54536451a4ec44d5fbee00989fb992
Yudai Kitano <kitano.prog@gmail.com>
2020-07-30 09:45:54 +0900

    feature: add navigation to new room

--- lib/presentation/notice_list/notice_list_model.dart
+++ lib/presentation/notice_list/notice_list_model.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:takutore/domain/notice.dart';
+import '../../domain/notice.dart';
+import '../../domain/room.dart';
+import '../../domain/user.dart';
 
 class NoticeListModel extends ChangeNotifier {
   FirebaseAuth _auth = FirebaseAuth.instance;
   Firestore _store = Firestore.instance;
   List<Notice> notices = [];
+  Room room;
   bool isLoading = false;
 
   void beginLoading() {
@@ -34,6 +37,9 @@ class NoticeListModel extends ChangeNotifier {
         (doc) async {
           final notice = Notice(doc);
           await notice.fetchSender();
+          if (notice.type == 'add room') {
+            await this.fetchRoom(notice: notice);
+          }
           return notice;
         },
       ),
@@ -42,5 +48,44 @@ class NoticeListModel extends ChangeNotifier {
     this.notices = notices;
 
     this.endLoading();
+  }
+
+  Future<User> _fetchUserFromFirebase({String uid}) async {
+    final document = _store.collection('users').document(uid);
+    final doc = await document.get();
+    final user = User(
+      uid: doc.documentID,
+      displayName: doc['displayName'],
+      photoURL: doc['photoURL'],
+      isTeacher: doc['isTeacher'],
+      createdAt: doc['createdAt'],
+    );
+    return user;
+  }
+
+  Future fetchRoom({Notice notice}) async {
+    final document = _store
+        .collection('users')
+        .document(notice.data['senderID'])
+        .collection('rooms')
+        .document(notice.data['roomID']);
+    final doc = await document.get();
+    final teacher =
+        await _fetchUserFromFirebase(uid: doc['member']['teacherID']);
+    final student =
+        await _fetchUserFromFirebase(uid: doc['member']['studentID']);
+    final room = Room(
+      documentId: doc.documentID,
+      teacher: teacher,
+      student: student,
+      lastMessage: doc['lastMessage'],
+      updatedAt: doc['updatedAt'],
+      createdAt: doc['createdAt'],
+      lastMessageFromUid: doc['lastMessageFromUid'],
+      hasNewMessage: doc['numNewMessage'].toDouble() > 0,
+      isAllow: doc['isAllow'],
+    );
+    this.room = room;
+    notifyListeners();
   }
 }
--- lib/presentation/notice_list/notice_list_page.dart
+++ lib/presentation/notice_list/notice_list_page.dart
@@ -1,65 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../domain/notice.dart';
+import '../chat_room/chat_room_page.dart';
 import 'notice_list_model.dart';
 
 class NoticeList extends StatelessWidget {
-  Widget _buildNoticeCell(BuildContext context, Notice notice) {
-    switch (notice.type) {
+  Widget _buildNoticeCell(
+      BuildContext context, int index, NoticeListModel model) {
+    switch (model.notices[index].type) {
       case 'add room':
-        return _buildAddRoomCell(context, notice);
+        return _buildAddRoomCell(context, index, model);
       default:
         return SizedBox();
     }
   }
 
-  Widget _buildAddRoomCell(BuildContext context, Notice notice) {
+  Widget _buildAddRoomCell(
+    BuildContext context,
+    int index,
+    NoticeListModel model,
+  ) {
+    final notice = model.notices[index];
+
     return Material(
       color: Colors.white,
       child: InkWell(
-        onTap: () {
-          // TODO: Navigation to NoticeDetail.
+        onTap: () async {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => ChatRoom(
+                user: notice.sender,
+                room: model.room,
+              ),
+            ),
+          );
         },
         child: Container(
           padding: EdgeInsets.all(15),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.transparent,
-                backgroundImage: NetworkImage(notice.sender.photoURL),
-              ),
-              SizedBox(width: 15),
               Flexible(
-                child: Column(
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black87,
-                        ),
-                        children: <InlineSpan>[
-                          TextSpan(
-                            text: notice.sender.displayName,
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      backgroundImage: NetworkImage(notice.sender.photoURL),
+                    ),
+                    SizedBox(width: 15),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black87,
+                              ),
+                              children: <InlineSpan>[
+                                TextSpan(
+                                  text: notice.sender.displayName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: 'さんが${notice.message}',
+                                ),
+                              ],
+                            ),
                           ),
-                          TextSpan(
-                            text: 'さんが${notice.message}',
+                          SizedBox(height: 5),
+                          Text(
+                            notice.createdAt,
+                            style: TextStyle(
+                              color: Colors.black54,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                    SizedBox(height: 5),
-                    Text(
-                      notice.createdAt,
-                      style: TextStyle(
-                        color: Colors.black54,
-                      ),
-                    ),
                   ],
                 ),
+              ),
+              SizedBox(width: 15),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 16,
+                color: Colors.black54,
               ),
             ],
           ),
@@ -92,16 +119,16 @@ class NoticeList extends StatelessWidget {
               );
             }
 
-            final notices = model.notices.map(
-              (notice) {
-                return _buildNoticeCell(context, notice);
-              },
-            ).toList();
-
             return ListView.separated(
-              itemBuilder: (context, index) => notices[index],
+              itemBuilder: (context, index) {
+                return _buildNoticeCell(
+                  context,
+                  index,
+                  model,
+                );
+              },
               separatorBuilder: (context, index) => Divider(height: 0.5),
-              itemCount: notices.length,
+              itemCount: model.notices.length,
             );
           },
         ),
