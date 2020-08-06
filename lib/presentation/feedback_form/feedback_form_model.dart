51da633b054aac144ec0d1e9ecfd61f68bcd5629
Yudai Kitano <kitano.prog@gmail.com>
2020-08-07 07:26:35 +0900

    add: generate feedback form page

---
+++ lib/presentation/feedback_form/feedback_form_model.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class FeedbackFormModel extends ChangeNotifier {
+  // TODO: write code here.
+}
---
+++ lib/presentation/feedback_form/feedback_form_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeedbackForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('フィードバック'),
+      ),
+      body: Container(),
+    );
+  }
+}
