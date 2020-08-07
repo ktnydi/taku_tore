8e05d815beeff61fe9b49ac75e2d400c34713e1b
Yudai Kitano <kitano.prog@gmail.com>
2020-08-07 14:18:59 +0900

    add: a feedback feature

--- lib/presentation/feedback_form/feedback_form_model.dart
+++ lib/presentation/feedback_form/feedback_form_model.dart
@@ -1,5 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class FeedbackFormModel extends ChangeNotifier {
-  // TODO: write code here.
+  TextEditingController controller = TextEditingController();
+  bool isLoading = false;
+
+  void beginLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future addFeedback({String content}) async {
+    final currentUser = await FirebaseAuth.instance.currentUser();
+
+    final webAppURL = DotEnv().env['GOOGLE_WEB_APP_FEEDBACK_URL'];
+
+    await http.post(
+      webAppURL,
+      body: {
+        'senderID': currentUser.uid,
+        'content': content,
+        'createdAt': DateTime.now().toString(),
+      },
+    );
+  }
 }
--- lib/presentation/feedback_form/feedback_form_page.dart
+++ lib/presentation/feedback_form/feedback_form_page.dart
@@ -1,13 +1,214 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'feedback_form_model.dart';
 
 class FeedbackForm extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<bool> _confirmDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('確認'),
+          content: Text('フィードバックを送信しますか？'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                'キャンセル',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+            FlatButton(
+              child: Text(
+                '送信',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              onPressed: () => Navigator.pop(context, true),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future _alertDialog(BuildContext context, {String errorText}) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('エラー'),
+          content: Text(errorText),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                'OK',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future _addFeedback(BuildContext context, {FeedbackFormModel model}) async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    try {
+      FocusScope.of(context).unfocus();
+
+      final isConfirm = await this._confirmDialog(context);
+
+      if (!isConfirm) return;
+
+      model.beginLoading();
+
+      await model.addFeedback(content: model.controller.text);
+
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('フィードバックを送信しました'),
+        ),
+      );
+
+      model.controller.clear();
+
+      model.endLoading();
+    } catch (e) {
+      model.endLoading();
+      this._alertDialog(
+        context,
+        errorText: e.toString(),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('フィードバック'),
+    return ChangeNotifierProvider<FeedbackFormModel>(
+      create: (_) => FeedbackFormModel(),
+      child: Consumer<FeedbackFormModel>(
+        builder: (_, model, __) {
+          return GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Stack(
+              children: <Widget>[
+                Scaffold(
+                  key: _scaffoldKey,
+                  appBar: AppBar(
+                    title: Text('フィードバック'),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          '送信',
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        textColor: Theme.of(context).primaryColor,
+                        disabledTextColor: Colors.black38,
+                        onPressed: () async {
+                          await _addFeedback(context, model: model);
+                        },
+                      ),
+                    ],
+                  ),
+                  body: SingleChildScrollView(
+                    reverse: true,
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      child: Form(
+                        key: _formKey,
+                        child: FormField<String>(
+                          autovalidate: false,
+                          initialValue: '',
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return '入力してください';
+                            }
+
+                            if (value.length > 800) {
+                              return '文字数が超過しています';
+                            }
+
+                            return null;
+                          },
+                          builder: (state) {
+                            return Column(
+                              children: <Widget>[
+                                TextField(
+                                  controller: model.controller,
+                                  autofocus: true,
+                                  decoration: InputDecoration(
+                                    hintText: '内容を入力',
+                                    enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: state.hasError
+                                            ? Colors.red
+                                            : Theme.of(context).disabledColor,
+                                      ),
+                                    ),
+                                  ),
+                                  minLines: 1,
+                                  maxLines: null,
+                                  style: TextStyle(
+                                    height: 1.5,
+                                  ),
+                                  onChanged: (value) {
+                                    state.didChange(value);
+                                  },
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Text(
+                                        state.errorText ?? '',
+                                        style: TextStyle(
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                    Text('${state.value.length}/800'),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                model.isLoading
+                    ? Container(
+                        color: Colors.white.withOpacity(0.6),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+          );
+        },
       ),
-      body: Container(),
     );
   }
 }
