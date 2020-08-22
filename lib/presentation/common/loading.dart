79dd5e8b4492ae0939d22f9872b53f60d29e7833
Yudai Kitano <kitano.prog@gmail.com>
2020-08-23 01:02:44 +0900

    modify: loading animation ui

--- lib/presentation/common/loading.dart
+++ lib/presentation/common/loading.dart
@@ -12,27 +12,9 @@ class Loading extends StatelessWidget {
     }
 
     return Container(
-      color: Colors.black12,
+      color: Colors.white.withOpacity(0.7),
       child: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                offset: Offset(0, 1),
-                blurRadius: 10,
-                spreadRadius: 0,
-              ),
-            ],
-          ),
-          child: Center(
-            child: CircularProgressIndicator(),
-          ),
-        ),
+        child: CircularProgressIndicator(),
       ),
     );
   }
