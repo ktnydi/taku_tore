0d67ba4b438af27770ca88aed00a30d67a5bf058
Yudai Kitano <kitano.prog@gmail.com>
2020-08-25 00:51:43 +0900

    add: update teacher feature

--- lib/presentation/teacher_edit_form/teacher_edit_form_model.dart
+++ lib/presentation/teacher_edit_form/teacher_edit_form_model.dart
@@ -1,9 +1,123 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:takutore/domain/teacher.dart';
 
 class TeacherEditFormModel extends ChangeNotifier {
   Teacher teacher;
+  String thumbnail;
+  TextEditingController title;
+  TextEditingController about;
+  TextEditingController canDo;
+  TextEditingController recommend;
+  File imageFile;
+  Uint8List imageData;
+  bool isLoading = false;
 
-  TeacherEditFormModel(this.teacher);
-  // TODO: 講師内容を更新する処理
+  TeacherEditFormModel(Teacher teacher) {
+    this.teacher = teacher;
+    this.thumbnail = teacher.thumbnail;
+    this.title = TextEditingController(text: teacher.title);
+    this.about = TextEditingController(text: teacher.about);
+    this.canDo = TextEditingController(text: teacher.canDo);
+    this.recommend = TextEditingController(text: teacher.recommend);
+  }
+
+  void dispose() {
+    super.dispose();
+
+    final controllers = [this.title, this.about, this.canDo, this.recommend];
+    controllers.forEach(
+      (controller) => controller.dispose(),
+    );
+  }
+
+  beginLoading() {
+    this.isLoading = true;
+    notifyListeners();
+  }
+
+  endLoading() {
+    this.isLoading = false;
+    notifyListeners();
+  }
+
+  bool disabled() {
+    final fields = [this.title, this.about, this.canDo, this.recommend];
+    final isComplete = fields.every((TextEditingController field) {
+      return 0 < field.text.length && field.text.length <= 500;
+    });
+    return !isComplete;
+  }
+
+  Future selectThumbnail() async {
+    final PickedFile pickedFile =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    if (pickedFile == null) {
+      return null;
+    }
+
+    final File croppedFile = await ImageCropper.cropImage(
+      sourcePath: pickedFile.path,
+      aspectRatio: CropAspectRatio(ratioX: 16, ratioY: 9),
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'Cropper',
+        toolbarColor: Colors.deepOrange,
+        toolbarWidgetColor: Colors.white,
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: true,
+      ),
+      iosUiSettings: IOSUiSettings(
+        minimumAspectRatio: 1.0,
+        cancelButtonTitle: 'キャンセル',
+        doneButtonTitle: '完了',
+      ),
+    );
+
+    if (croppedFile == null) {
+      return;
+    }
+
+    this.imageFile = croppedFile;
+    this.imageData = await croppedFile.readAsBytes();
+
+    notifyListeners();
+  }
+
+  Future updateTeacher() async {
+    final user = await FirebaseAuth.instance.currentUser();
+
+    if (imageData != null) {
+      // Firebase Storageに画像をアップロード
+      final path = '/images/${user.uid}_thumbnail.jpg';
+      final StorageReference storageRef =
+          FirebaseStorage.instance.ref().child(path);
+
+      // 以下をを指定しないとiOSではcontentTypeがapplication/octet-streamになる。
+      final metaData = StorageMetadata(contentType: "image/jpg");
+      final StorageUploadTask uploadTask = storageRef.putData(
+        this.imageData,
+        metaData,
+      );
+
+      // 画像の保存完了時にFirebaseにURLを保存する。
+      StorageTaskSnapshot snapshot = await uploadTask.onComplete;
+      this.thumbnail = await snapshot.ref.getDownloadURL();
+    }
+
+    final doc = Firestore.instance.collection('users').document(user.uid);
+    await doc.updateData({
+      'thumbnail': this.thumbnail,
+      'title': this.title.text,
+      'canDo': this.canDo.text,
+      'recommend': this.recommend.text,
+      'about': this.about.text,
+    });
+  }
 }
--- lib/presentation/teacher_edit_form/teacher_edit_form_page.dart
+++ lib/presentation/teacher_edit_form/teacher_edit_form_page.dart
@@ -7,19 +7,381 @@ class TeacherEditForm extends StatelessWidget {
   TeacherEditForm(this.teacher);
 
   final Teacher teacher;
+  final _formKey = GlobalKey<FormState>();
+
+  Future updateTeacher(context, TeacherEditFormModel model) async {
+    try {
+      if (_formKey.currentState.validate()) {
+        model.beginLoading();
+
+        await model.updateTeacher();
+
+        model.endLoading();
+
+        Navigator.pop(context);
+      }
+    } catch (error) {
+      model.endLoading();
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(error.toString()),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<TeacherEditFormModel>(
       create: (_) => TeacherEditFormModel(teacher),
       child: Consumer<TeacherEditFormModel>(
         builder: (_, model, __) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('講義内容の編集'),
-            ),
+          return Stack(
+            children: <Widget>[
+              Scaffold(
+                appBar: AppBar(
+                  title: Text('講義内容の編集'),
+                ),
+                body: SingleChildScrollView(
+                  child: SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: <Widget>[
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Thumbnail(),
+                                SizedBox(height: 20),
+                                Title(),
+                                SizedBox(height: 20),
+                                CanDo(),
+                                SizedBox(height: 20),
+                                Recommend(),
+                                SizedBox(height: 20),
+                                About(),
+                                SizedBox(height: 20),
+                                ButtonTheme(
+                                  minWidth: double.infinity,
+                                  height: 50,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: FlatButton(
+                                    color: Theme.of(context).primaryColor,
+                                    child: Text(
+                                      '講義内容を更新する',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    disabledColor: Colors.black38,
+                                    onPressed: !model.disabled()
+                                        ? () async {
+                                            this.updateTeacher(context, model);
+                                          }
+                                        : null,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              model.isLoading
+                  ? Container(
+                      color: Colors.white.withOpacity(0.7),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : SizedBox(),
+            ],
           );
         },
       ),
+    );
+  }
+}
+
+class Thumbnail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'サービスサムネイル',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 10),
+        Consumer<TeacherEditFormModel>(
+          builder: (_, model, __) {
+            return InkWell(
+              onTap: () async {
+                // TODO: select thumbnail image
+                await model.selectThumbnail();
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.05),
+                    ),
+                    child: model.imageFile != null
+                        ? Image.file(
+                            model.imageFile,
+                            fit: BoxFit.cover,
+                          )
+                        : Center(
+                            child: model.teacher.thumbnail.isNotEmpty
+                                ? Image.network(model.thumbnail)
+                                : Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.image,
+                                        size: 50,
+                                        color: Colors.black38,
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        'タップして画像を選択',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black38,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TeacherEditFormModel>(
+      builder: (_, model, __) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'サービスタイトル',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: model.title,
+              validator: (value) {
+                if (value.trim().isEmpty) {
+                  return '入力してください。';
+                }
+                if (value.length > 80) {
+                  return '80文字以内にしてください。';
+                }
+                return null;
+              },
+              minLines: 1,
+              maxLines: 10,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'タイトル',
+                filled: true,
+                fillColor: Colors.black.withOpacity(0.05),
+              ),
+              maxLength: 80,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class About extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TeacherEditFormModel>(
+      builder: (_, model, __) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '自己紹介',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: model.about,
+              validator: (value) {
+                if (value.trim().isEmpty) {
+                  return '入力してください。';
+                }
+                if (value.length > 500) {
+                  return '500文字以内にしてください。';
+                }
+                return null;
+              },
+              minLines: 5,
+              maxLines: 10,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: '経歴、趣味など',
+                filled: true,
+                fillColor: Colors.black.withOpacity(0.05),
+              ),
+              maxLength: 500,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class CanDo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TeacherEditFormModel>(
+      builder: (_, model, __) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'サービス内容',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: model.canDo,
+              validator: (value) {
+                if (value.trim().isEmpty) {
+                  return '入力してください。';
+                }
+                if (value.length > 500) {
+                  return '500文字以内にしてください。';
+                }
+                return null;
+              },
+              minLines: 5,
+              maxLines: 10,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: '内容',
+                filled: true,
+                fillColor: Colors.black.withOpacity(0.05),
+              ),
+              maxLength: 500,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Recommend extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TeacherEditFormModel>(
+      builder: (_, model, __) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'こんな方におすすめ',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: model.recommend,
+              validator: (value) {
+                if (value.trim().isEmpty) {
+                  return '入力してください。';
+                }
+                if (value.length > 500) {
+                  return '500文字以内にしてください。';
+                }
+                return null;
+              },
+              minLines: 5,
+              maxLines: 10,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'おすすめのユーザー',
+                filled: true,
+                fillColor: Colors.black.withOpacity(0.05),
+              ),
+              maxLength: 500,
+            ),
+          ],
+        );
+      },
     );
   }
 }
