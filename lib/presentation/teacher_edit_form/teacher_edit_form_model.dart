621bcf708be64af664b36232b6ab9e48711a4db2
Yudai Kitano <kitano.prog@gmail.com>
2020-08-24 22:24:26 +0900

    add: teacher edit form page

---
+++ lib/presentation/teacher_edit_form/teacher_edit_form_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:takutore/domain/teacher.dart';
+
+class TeacherEditFormModel extends ChangeNotifier {
+  Teacher teacher;
+
+  TeacherEditFormModel(this.teacher);
+  // TODO: 講師内容を更新する処理
+}
