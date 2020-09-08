87c68be7b6b6f5ab46ef0f4a742897f6591d3056
Yudai Kitano <kitano.prog@gmail.com>
2020-09-08 10:45:06 +0900

    add: check supoport version feature

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,9 +1,25 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
+import 'package:version/version.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class MainModel extends ChangeNotifier {
   bool isRequiredUpdate = false;
 
   Future<void> checkVersion() async {
-    // TODO: check whether update is needed or not.
+    final configName =
+        Platform.isIOS ? 'support_version_ios' : 'support_version_android';
+    final remoteConfig = await RemoteConfig.instance;
+    await remoteConfig.fetch(expiration: const Duration(seconds: 0));
+    await remoteConfig.activateFetched();
+    final supportVersion = Version.parse(remoteConfig.getString(configName));
+    final packageInfo = await PackageInfo.fromPlatform();
+    final currentVersion = Version.parse(packageInfo.version);
+
+    if (currentVersion < supportVersion) {
+      this.isRequiredUpdate = true;
+      notifyListeners();
+    }
   }
 }
